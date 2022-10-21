eed77fc7b63b1729f8ecb05a4ef3befe60d32026
ztursunov <ztursunov@ivi.ru>
2022-10-22 00:27:18 +0300

    task1 completed

---
+++ task1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+while [ -n "$1" ]
+do
+case "$1" in
+--workers) workers="$2" 
+echo "$1 activated with param $workers"
+shift ;;
+--column_links) column_links="$2" 
+echo "$1 activated with param $column_links" 
+shift ;;
+--output_folder) output_folder="$2"
+echo "$1 activated with param $output_folder"
+shift ;;
+*) echo "$1 is not an option" ;;
+esac
+shift
+done
+
+cat dataset.csv | awk -v column="$column_links" -F ';' 'NR>1 {print $column}' | parallel -j $workers wget -P ./$output_folder -p -q {} 
