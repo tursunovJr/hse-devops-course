57e1d286df72f5feaf0d06721dfd523f81959043
ztursunov <ztursunov@ivi.ru>
2022-11-05 23:56:01 +0500

    task2 modified

--- run2.sh
+++ run2.sh
@@ -3,19 +3,14 @@ while [ -n "$1" ]
 do
 case "$1" in
 --input) input_param="$2" 
-echo "$1 activated with param $input_param"
 shift ;;
 --shuffle) shuffle_param="$2" 
-echo "$1 activated with param $shuffle_param" 
 shift ;;
 --train_ratio) train_param="$2"
-echo "$1 activated with param $train_param"
 shift ;;
 --save_train) save_train_param="$2"
-echo "$1 activated with param $save_train_param"
 shift ;;
 --save_val) save_val_param="$2"
-echo "$1 activated with param $save_val_param"
 shift ;;
 *) echo "$1 is not an option" ;;
 esac
@@ -28,10 +23,9 @@ train_ratio=$(($number_of_rows * $train_param / 100))
 val_ratio=$((train_ratio + 1))
 
 if [[ "$shuffle_param" == 1 ]]; then
-    cat $input_param | gshuf > $input_param/new_dataset.csv
-    cat $input_param/new_dataset.csv | sed -n "2,$train_ratio p" >> $save_train_param
-    cat $input_param/new_dataset.csv | sed -n "$val_ratio,$number_of_rows p" >> $save_val_param
-    rm $input_param
+    cat $input_param | gshuf > ./test.csv
+    cat ./test.csv | sed -n "2,$train_ratio p" >> $save_train_param
+    cat ./test.csv | sed -n "$val_ratio,$number_of_rows p" >> $save_val_param
 else
     cat $input_param | sed -n "2,$train_ratio p" >> $save_train_param
     cat $input_param | sed -n "$val_ratio,$number_of_rows p" >> $save_val_param
